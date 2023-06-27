faae04e02a3acaf308ab204f27d210b654542966
Pavlo Yefimov2 <pavlo_efimov@ukr.net>
2023-06-27 21:48:09 +0300

    fetch adding3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { Configuration, OpenAIApi } from 'openai'
+// import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// })
 
-const openai = new OpenAIApi(configuration)
+// const openai = new OpenAIApi(configuration)
 
 const chatbotConversation = document.getElementById('chatbot-conversation')
  
