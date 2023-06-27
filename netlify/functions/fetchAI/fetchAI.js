fcfea9f12e928afa9de390afe7ef697092f6651a
Pavlo Yefimov2 <pavlo_efimov@ukr.net>
2023-06-27 22:36:39 +0300

    fetch adding11

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -42,21 +42,21 @@ const openai = new OpenAIApi(configuration)
 
 const handler = async (event) => {
     try {
-        // const response = await openai.createCompletion({
-        //     model: 'davinci:ft-scrimba-2023-03-30-23-10-03',
-        //     prompt: event.body,
-        //     max_tokens: 500,
-        //     presence_penalty: 0.3,
-        //     frequency_penalty: 0,
-        //     temperature: 0,
-        //     stop: ['\n', '->']
-        // })
+        const response = await openai.createCompletion({
+            model: 'davinci:ft-pavlolearn-2023-06-26-19-19-56',
+            prompt: event.body,
+            max_tokens: 500,
+            presence_penalty: 0.3,
+            frequency_penalty: 0,
+            temperature: 0,
+            stop: ['\n', '->']
+        })
         return {
             statusCode: 200,
             body: JSON.stringify({
                
-              // reply: response.data
-              message: 'hello world'
+              reply: response.data
+              // message: 'hello world'
 
             })
         }
